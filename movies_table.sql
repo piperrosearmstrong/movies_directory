f9c4960d5fb7aa99ae413daf0a55917aefb60990
Piper-Rose Armstrong <piperrosearmstrong@gmail.com>
2022-09-29 11:38:56 +0100

    Designing a schema with one table Challenge Updated

--- movies_table.sql
+++ movies_table.sql
@@ -1,6 +1,9 @@
-CREATE TABLE albums (
+CREATE TABLE movies (
   id SERIAL PRIMARY KEY,
   title text,
   genre text,
   release_year int
 );
+
+INSERT INTO "public"."movies" ("id", "title", "genre", "release_year") VALUES
+(1, 'The Lord of the Rings: The Fellowship of the Ring','High Fantasy', 2001);
